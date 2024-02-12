c110e338bf818c77ad59524af7a5fc0177ff9076
glacialcascade <11969863+glacialcascade@users.noreply.github.com>
2024-02-11 21:46:23 -0500

    Create a lot of classes

--- Target Practice/Assets/Scripts/Infrastructure/SetupStage/CommandMakeUnit.cs
+++ Target Practice/Assets/Scripts/Infrastructure/SetupStage/CommandMakeUnit.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Publisher : MonoBehaviour
+public class CommandMakeUnit : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
