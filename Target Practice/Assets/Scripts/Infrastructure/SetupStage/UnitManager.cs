a39737162e9f0de986e74582618a077027d6e5d8
seognaMcitpyrC@gmail.com <jerlee25@bergen.org>
2024-02-22 00:45:09 -0500

    Made unit types and sort of gui (press space)

--- Target Practice/Assets/Scripts/Infrastructure/SetupStage/UnitManager.cs
+++ Target Practice/Assets/Scripts/Infrastructure/SetupStage/UnitManager.cs
@@ -1,18 +1,37 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
+
 
 public class UnitManager : MonoBehaviour
 {
     // Start is called before the first frame update
+    public GameObject[] units;
+    public float SpawnXRange = 5.0f;
+    public float SpawnYRange = 5.0f;
+    public float FirstSpawnTime = 2.0f;
+    public float SpawnIntervalConst = 1.0f;
+    public bool isSummoning = true;
+
     void Start()
     {
-        
+        InvokeRepeating("Spawn", FirstSpawnTime, SpawnIntervalConst);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
+    }
+
+    
+    public void Spawn()
+    {
+        if (isSummoning)
+        {
+            Instantiate(units[UnityEngine.Random.Range(0, units.Length)], new Vector3(UnityEngine.Random.Range(-SpawnXRange, SpawnXRange), UnityEngine.Random.Range(-SpawnYRange, SpawnYRange), 0), Quaternion.identity);
+        }
     }
 }
