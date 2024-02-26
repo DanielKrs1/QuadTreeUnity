eacbaccbbcdca81d39fc2ab9902c277a7b4987cb
seognaMcitpyrC@gmail.com <jerlee25@bergen.org>
2024-02-26 17:49:44 -0500

    silly little choose where to buy

--- Target Practice/Assets/Scripts/Infrastructure/SetupStage/SetupUI.cs
+++ Target Practice/Assets/Scripts/Infrastructure/SetupStage/SetupUI.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class SetupUI : MonoBehaviour
 {
     // TODO: Everything. I have legitimately no idea how to do UI pain
+
+    // I mean it's sort of done now? Like it's not good but there's some UI 
+     
     // Start is called before the first frame update
     void Start()
     {
